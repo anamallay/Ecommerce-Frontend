b5ccb83ad125210d44a3960dcdbfc7d714095b9f
Amal Khardli <amalalikhardli@gmail.com>
2024-02-26 23:51:15 +0300

    commit 29

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import pluginRewriteAll from "vite-plugin-rewrite-all";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), pluginRewriteAll()]
+  plugins: [react(), pluginRewriteAll()],
+  build: { manifest: true, outDir: "./dist" },
+  base: "/",
+  root: "./src",
 });
